2174fe7d2b78f533ab6bfa366c930f2a5565a50e
IuriGP <iuri.gp@sempreceub.com>
2023-05-23 21:22:20 -0300

    .

--- q5/q5.go
+++ q5/q5.go
@@ -1,5 +1,7 @@
 package q5
 
+import "fmt"
+
 //Você está desenvolvendo um sistema de gerenciamento de vendas para uma loja. Cada venda possui um código único, data,
 //valor total e uma lista de produtos vendidos. Cada produto tem um código, nome e preço unitário. Você decidiu usar
 //structs para representar as informações de cada venda e de cada produto.
@@ -13,6 +15,7 @@ package q5
 
 type Product struct {
 	Code  string
+	Date  int
 	Name  string
 	Price float64
 }
@@ -23,6 +26,43 @@ type Sale struct {
 }
 
 func CalculateTotalSales(sales map[string]*Sale) float64 {
-	// Seu código aqui
-	return 0
+	total := 0.0
+	for _, Sale := range sales {
+		sum := 0.0
+		for _, Product := range Sale.Products {
+			sum += Product.Price
+		}
+		total += sum
+	}
+	return total
+}
+
+func main() {
+	sale1 := &Sale{
+		Code: "S001",
+		//Date: "2023-05-01",
+		Products: []*Product{
+			&Product{Code: "P001", Name: "Product 1", Price: 50.0},
+			&Product{Code: "P002", Name: "Product 2", Price: 25.0},
+			&Product{Code: "P003", Name: "Product 3", Price: 25.0},
+		},
+	}
+
+	sale2 := &Sale{
+		Code: "S002",
+		//Date: "2023-05-05",
+		Products: []*Product{
+			&Product{Code: "P004", Name: "Product 4", Price: 25.0},
+			&Product{Code: "P005", Name: "Product 5", Price: 50.0},
+		},
+	}
+
+	sales := map[string]*Sale{
+		"S001": sale1,
+		"S002": sale2,
+	}
+
+	total := CalculateTotalSales(sales)
+	fmt.Println(total)
+
 }
